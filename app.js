8b3e00d1a95d54330a406e10ec1ee8153a605fda
alejandro11k <k11alejandro@gmail.com>
2017-10-04 09:45:13 -0300

    demo

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 var storage = require ("storage-device-info");
 var processWindows = require ("node-process-windows");
-var program = "C:\\Windows\\System32\\calc.exe"
+//var program = "C:\\Windows\\System32\\calc.exe"
+var program = "C:\\Users\\alejandroK\\dev\\nw-loader\\build\\nw-loader\\win64\\nw-loader.exe --url http://www.exo.com.ar"
 var cp = require("child_process");
+var processName = "nw-loader"
+var mainWindowTitle = "EXO S.A. | Soluciones Tecnol�gicas"
 //cp.exec(program); // notice this without a callback..
 //process.exit(0); // exit this nodejs process
 
@@ -40,12 +43,13 @@ var processes = processWindows.getProcesses(function(err, processes){
 	
 	//console.log((getProcess(processes,"Calculator")).pid)
 	
-	let found = (getProcess(processes,"ApplicationFrameHost")).pid
-    processWindows.focusWindow(6724);
+	//let found = (getProcess(processes,"ApplicationFrameHost")).pid
+   // processWindows.focusWindow(6724);
 
-	if (getProcess(processes,"Calculator") !== undefined) {
+	if (getProcess(processes, processName, mainWindowTitle) !== undefined) {
 	  console.log("found")
-	  let found = (getProcess(processes,"Calculator")).pid
+		let found = (getProcess(processes,processName, mainWindowTitle)).pid
+		console.log(found);
 	  processWindows.focusWindow(found);
 	} else {
 	  console.log("not found")
@@ -55,21 +59,20 @@ var processes = processWindows.getProcesses(function(err, processes){
 			console.error(err);
 			return;
 		}
-		console.log(stdout);
+		//console.log(stdout);
 		process.exit(0);// exit process once it is opened
 	})
 
 	}
-
 		//process.exit(0);
 	});
-	
-	
+		
 	//processWindows.focusWindow("Calculator");
 
-	
-function getProcess(processes, process) {
-  return processes.find((elem) => { return elem.processName === process  }) // && elem.mainWindowTitle !== "" })	
+function getProcess(processes, processName, mainWindowTitle) {
+  return processes.find((elem) => { 
+		return elem.processName === processName 
+		&& elem.mainWindowTitle === mainWindowTitle })	
 }
 	
 })
